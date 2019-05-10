ad640c906a7249871441ab35d109c28d1393aa31
GuilleAntico <antico.guillermo@gmail.com>
2019-05-10 18:23:10 -0300

    adding test cases

--- Farmer.js
+++ Farmer.js
@@ -16,9 +16,9 @@ class Farmer {
                 if(farmer.farms.length){
                     farmer.farms.forEach( farm => {
                         // filter based on cropType and year condition
-                        const result = farm.plantingEvents.filter( pe => pe.cropType === cropType && this.checkIfSameYear(year, pe.date));
+                        const result = farm.plantingEvents.filter( pe => pe.cropType === cropType && this._checkIfSameYear(year, pe.date));
                         // now i want to sort Desc the planting events so i can be sure the first item is the most recent
-                        if(result && result.length) plantingEvents = this.sortPlantingEventDesc(result)
+                        if(result && result.length) plantingEvents = this._sortPlantingEventDesc(result)
                     });
                 }
                 if(plantingEvents.length){
@@ -29,25 +29,28 @@ class Farmer {
                 }
             });
             // here i want sort the names based on plantingDate
-            return this.sortNamesByPlantingDateDesc(names);
+            return this._sortNamesByPlantingDateDesc(names);
         }catch(error){
             throw error;
         }
     }
-    sortPlantingEventDesc(plantingEvents){
+    // naming convention for internal class method
+    _sortPlantingEventDesc(plantingEvents){
         return plantingEvents.sort( (a,b)=>{
             const dateA = new Date(a.date), dateB = new Date(b.date);
             return dateB - dateA;
         })
     }
-    sortNamesByPlantingDateDesc(farmers){
+    // naming convention for internal class method
+    _sortNamesByPlantingDateDesc(farmers){
         return farmers.sort( (a, b)=> {
             if (new Date(a.plantingEvents[0].date) < new Date(b.plantingEvents[0].date)) return 1;
             if (new Date(a.plantingEvents[0].date) > new Date(b.plantingEvents[0].date)) return -1;
             return 0;
         })
     }
-    checkIfSameYear(year, date){
+    // naming convention for internal class method
+    _checkIfSameYear(year, date){
         const formattedYear = new Date(date).getFullYear();
         return (formattedYear === year);
     }
--- tests/unit/Farmer.test.js
+++ tests/unit/Farmer.test.js
@@ -14,122 +14,217 @@ describe('Farmer class ', () =>{
         sandbox.restore();
     })
 
-    it('should return sorted values correctly', ()=>{
-        structure = [
-            {
-                name: 'Test A',
-                farms: [
-                    {
-                        plantingEvents: [
-                            {
-                                date: '2019-05-12 00:00:00',
-                                cropType: 'corn'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                name: 'Test B',
-                farms: [
-                    {
-                        plantingEvents: [
-                            {
-                                date: '2019-05-30 04:00:00',
-                                cropType: 'corn'
-                            }
-                        ]
-                    },
-                ]
-            }
-        ];
-        const expectedValue = [
-            {
-                name: 'Test B',
-                plantingEvents: [
-                    {
-                        date: '2019-05-30 04:00:00',
-                        cropType: 'corn'
-                    }
-                ]
-            },
-            {
-                name: 'Test A',
-                plantingEvents: [
-                    {
-                        date: '2019-05-12 00:00:00',
-                        cropType: 'corn'
-                    }
-                ]
-            }
-        ];
-        farmerInstance = new Farmer(structure);
-        const checkIfSameYearSpy = sandbox.spy(farmerInstance, "checkIfSameYear");
-        const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "sortPlantingEventDesc");
-        const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "sortNamesByPlantingDateDesc");
-        const result = farmerInstance.filterByCropType('corn', 2019);
-        expect(result).to.deep.equal(expectedValue);
-        sandbox.assert.calledTwice(checkIfSameYearSpy);
-        sandbox.assert.calledTwice(sortPlantingEventDescSpy);
-        sandbox.assert.calledOnce(sortNamesByPlantingDateDescSpy);
-    });
+    describe('filterByCropType', ()=>{
+        it('should return sorted values correctly', ()=>{
+            structure = [
+                {
+                    name: 'Test A',
+                    farms: [
+                        {
+                            plantingEvents: [
+                                {
+                                    date: '2019-05-12 00:00:00',
+                                    cropType: 'corn'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: 'Test B',
+                    farms: [
+                        {
+                            plantingEvents: [
+                                {
+                                    date: '2019-05-30 04:00:00',
+                                    cropType: 'corn'
+                                }
+                            ]
+                        },
+                    ]
+                }
+            ];
+            const expectedValue = [
+                {
+                    name: 'Test B',
+                    plantingEvents: [
+                        {
+                            date: '2019-05-30 04:00:00',
+                            cropType: 'corn'
+                        }
+                    ]
+                },
+                {
+                    name: 'Test A',
+                    plantingEvents: [
+                        {
+                            date: '2019-05-12 00:00:00',
+                            cropType: 'corn'
+                        }
+                    ]
+                }
+            ];
+            farmerInstance = new Farmer(structure);
+            const checkIfSameYearSpy = sandbox.spy(farmerInstance, "_checkIfSameYear");
+            const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "_sortPlantingEventDesc");
+            const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "_sortNamesByPlantingDateDesc");
+            const result = farmerInstance.filterByCropType('corn', 2019);
+            expect(result).to.deep.equal(expectedValue);
+            sandbox.assert.calledTwice(checkIfSameYearSpy);
+            sandbox.assert.calledTwice(sortPlantingEventDescSpy);
+            sandbox.assert.calledOnce(sortNamesByPlantingDateDescSpy);
+        });
     
-    it('should throw an error if cropType is invalid', ()=>{
-        farmerInstance = new Farmer();
-        const checkIfSameYearSpy = sandbox.spy(farmerInstance, "checkIfSameYear");
-        const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "sortPlantingEventDesc");
-        const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "sortNamesByPlantingDateDesc");
-        try{
-          farmerInstance.filterByCropType('somethingElse', 2019);
-        }catch(e){
-            console.log(e.message);
-            expect(e).to.be.instanceof(Error);
-            expect(e.message).to.equal('Invalid cropType, must be corn or soybean');
+        it('should throw an error if cropType is invalid', ()=>{
+            farmerInstance = new Farmer();
+            const checkIfSameYearSpy = sandbox.spy(farmerInstance, "_checkIfSameYear");
+            const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "_sortPlantingEventDesc");
+            const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "_sortNamesByPlantingDateDesc");
+            try{
+                farmerInstance.filterByCropType('somethingElse', 2019);
+            }catch(e){
+                console.log(e.message);
+                expect(e).to.be.instanceof(Error);
+                expect(e.message).to.equal('Invalid cropType, must be corn or soybean');
+                sandbox.assert.notCalled(checkIfSameYearSpy);
+                sandbox.assert.notCalled(sortPlantingEventDescSpy);
+                sandbox.assert.notCalled(sortNamesByPlantingDateDescSpy);
+            }
+        });
+        it('should throw an error if year is invalid', ()=>{
+            farmerInstance = new Farmer();
+            const checkIfSameYearSpy = sandbox.spy(farmerInstance, "_checkIfSameYear");
+            const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "_sortPlantingEventDesc");
+            const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "_sortNamesByPlantingDateDesc");
+            try{
+                farmerInstance.filterByCropType('soybean', 'invalid');
+            }catch(e){
+                console.log(e.message);
+                expect(e).to.be.instanceof(Error);
+                expect(e.message).to.equal('Invalid year format');
+                sandbox.assert.notCalled(checkIfSameYearSpy);
+                sandbox.assert.notCalled(sortPlantingEventDescSpy);
+                sandbox.assert.notCalled(sortNamesByPlantingDateDescSpy);
+            }
+        });
+        it('should return empty if all farms are empty', ()=>{
+            structure = [
+                {
+                    name: 'Test A',
+                    farms: []
+                },
+                {
+                    name: 'Test B',
+                    farms: []
+                }
+            ];
+            const expectedValue = [];
+            farmerInstance = new Farmer(structure);
+            const checkIfSameYearSpy = sandbox.spy(farmerInstance, "_checkIfSameYear");
+            const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "_sortPlantingEventDesc");
+            const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "_sortNamesByPlantingDateDesc");
+            const result = farmerInstance.filterByCropType('corn', 2019);
+            expect(result).to.deep.equal(expectedValue);
             sandbox.assert.notCalled(checkIfSameYearSpy);
             sandbox.assert.notCalled(sortPlantingEventDescSpy);
-            sandbox.assert.notCalled(sortNamesByPlantingDateDescSpy);
-        }
-    });
-    it('should throw an error if year is invalid', ()=>{
-        farmerInstance = new Farmer();
-        const checkIfSameYearSpy = sandbox.spy(farmerInstance, "checkIfSameYear");
-        const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "sortPlantingEventDesc");
-        const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "sortNamesByPlantingDateDesc");
-        try{
-            farmerInstance.filterByCropType('soybean', 'invalid');
-        }catch(e){
-            console.log(e.message);
-            expect(e).to.be.instanceof(Error);
-            expect(e.message).to.equal('Invalid year format');
-            sandbox.assert.notCalled(checkIfSameYearSpy);
-            sandbox.assert.notCalled(sortPlantingEventDescSpy);
-            sandbox.assert.notCalled(sortNamesByPlantingDateDescSpy);
-        }
-    });
-    it('should return empty if all farms are empty', ()=>{
-        structure = [
-            {
-                name: 'Test A',
-                farms: []
-            },
-            {
-                name: 'Test B',
-                farms: []
-            }
-        ];
-        const expectedValue = [];
-        farmerInstance = new Farmer(structure);
-        const checkIfSameYearSpy = sandbox.spy(farmerInstance, "checkIfSameYear");
-        const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "sortPlantingEventDesc");
-        const sortNamesByPlantingDateDescSpy = sandbox.spy(farmerInstance, "sortNamesByPlantingDateDesc");
-        const result = farmerInstance.filterByCropType('corn', 2019);
-        expect(result).to.deep.equal(expectedValue);
-        sandbox.assert.notCalled(checkIfSameYearSpy);
-        sandbox.assert.notCalled(sortPlantingEventDescSpy);
-        sandbox.assert.calledOnce(sortNamesByPlantingDateDescSpy);
-    });
+            sandbox.assert.calledOnce(sortNamesByPlantingDateDescSpy);
+        });
     
-    describe('sortPlantingEventDesc', ()=>{
-        it('should')
+    })
+    describe('_sortPlantingEventDesc', ()=>{
+        // since these are internal methods of the class i only test the okay result.
+        it('should sort data correctly', ()=>{
+            const plantingEvents = [
+                {
+                    date: '2018-03-12 14:00:00',
+                    cropType: 'soybean'
+                },
+                {
+                    date: '2018-03-20 05:00:00',
+                    cropType: 'corn'
+                },
+            ];
+            const expectedValue = [
+                {
+                    date: '2018-03-20 05:00:00',
+                    cropType: 'corn'
+                },
+                {
+                    date: '2018-03-12 14:00:00',
+                    cropType: 'soybean'
+                },
+            ];
+            farmerInstance = new Farmer();
+            const sortPlantingEventDescSpy = sandbox.spy(farmerInstance, "_sortPlantingEventDesc");
+            const result = farmerInstance._sortPlantingEventDesc(plantingEvents);
+            expect(result).to.deep.equal(expectedValue);
+            sandbox.assert.calledOnce(sortPlantingEventDescSpy);
+            
+        })
+    });
+    describe('_checkIfSameYear', ()=>{
+        // since these are internal methods of the class i only test the okay result.
+        it('should return true if matches year', ()=>{
+            const year = 2019;
+            farmerInstance = new Farmer();
+            const checkIfSameYearSpy = sandbox.spy(farmerInstance, "_checkIfSameYear");
+            const result = farmerInstance._checkIfSameYear(year, new Date());
+            expect(result).to.equal(true);
+            sandbox.assert.calledOnce(checkIfSameYearSpy);
+        });
+        it('should return false if matches year', ()=>{
+            const year = 2018;
+            farmerInstance = new Farmer();
+            const checkIfSameYearSpy = sandbox.spy(farmerInstance, "_checkIfSameYear");
+            const result = farmerInstance._checkIfSameYear(year, new Date());
+            expect(result).to.equal(false);
+            sandbox.assert.calledOnce(checkIfSameYearSpy);
+        })
+    });
+    describe('_sortNamesByPlantingDateDesc', ()=>{
+        // since these are internal methods of the class i only test the okay result.
+        it('should return sorted names', ()=>{
+            const farmers = [
+                {
+                    plantingEvents: [
+                        {
+                            date: '2019-05-12 00:00:00',
+                            cropType: 'corn'
+                        }
+                    ]
+                },
+                {
+                    plantingEvents: [
+                        {
+                            date: '2019-05-30 04:00:00',
+                            cropType: 'corn'
+                        }
+                    ]
+                },
+            ];
+            const expectedValue = [
+                {
+                    plantingEvents: [
+                        {
+                            date: '2019-05-30 04:00:00',
+                            cropType: 'corn'
+                        }
+                    ]
+                },
+                {
+                    plantingEvents: [
+                        {
+                            date: '2019-05-12 00:00:00',
+                            cropType: 'corn'
+                        }
+                    ]
+                },
+            ]
+            farmerInstance = new Farmer(farmers);
+            const _sortNamesByPlantingDateDesc = sandbox.spy(farmerInstance, "_sortNamesByPlantingDateDesc");
+            const result = farmerInstance._sortNamesByPlantingDateDesc(farmers);
+            expect(result).to.deep.equal(expectedValue);
+            sandbox.assert.calledOnce(_sortNamesByPlantingDateDesc);
+        })
     })
 });
